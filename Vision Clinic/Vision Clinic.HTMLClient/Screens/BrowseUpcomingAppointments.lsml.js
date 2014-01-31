6c023d9f86d61a7a6a15fc7c9d8d24f768e4956e
Shane Poppleton <shane@bjja.com.au>
2014-01-31 15:33:02 +1000

    Finished Walkthrough

---
+++ Vision Clinic/Vision Clinic.HTMLClient/Screens/BrowseUpcomingAppointments.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.BrowseUpcomingAppointments.created = function (screen) {
+    // Write code here.
+    screen.details.displayName = "Appointments";
+};
